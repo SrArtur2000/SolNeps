d3d4f140dcd439cea5ff87c509d453c9205bf6ec
SrArtur2000 <61886899+SrArtur2000@users.noreply.github.com>
2020-08-04 20:57:13 -0300

    Update titulo.cc

--- titulo.cc
+++ titulo.cc
@@ -5,21 +5,21 @@ using namespace std;
 string title(string F){
 	if(F[0] >= 'a' and F[0] <= 'z'){
 		F[0] = char(F[0] - 'a' + 'A');
-	}
+	}// Muda a primeira letra de minúscula para maiuscula sempre
 	for(int i = 1 ; i < F.size() ; i++){
 		if(F[i] == ' '){
 			if(F[i+1] >= 'a' and F[i+1] <= 'z'){
 			F[i+1] = char(F[i +1] - 'a' + 'A');
-			}
+			}// Analise a letra depois de um espaço e a torna maiuscula
 		} else if(F[i] != ' ' and F[i - 1] != ' '){
 			if(F[i] >= 'A' and F[i] <= 'Z'){
 				F[i] = char(F[i] - 'A' + 'a');
 			}
-		}
+		}// torna as letras entre os espaços, sem ser a primeira, em minusculas
 	}
 
 return F;
-}
+} 
 
 int main(){ 
 	string F;
