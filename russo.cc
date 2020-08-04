c01c52016764f96f81454f676151bb54711e4b30
SrArtur2000 <61886899+SrArtur2000@users.noreply.github.com>
2020-08-04 20:48:07 -0300

    Update russo.cc

--- russo.cc
+++ russo.cc
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
+// Essa eu fiquei orgulhoso :D
 int main(){
 	int n;
 	cin>>n;
@@ -16,8 +16,9 @@ int main(){
 		cin>>v[i];
 		A[i] = v[i];
 		ordem[i] = 0;
-	}
-	sort(v, v + n);
+	}// Entra com o vetor que define as alturas das bonecas na ordem que elas estão
+	// Cria um vetor auxiliar A[i] para salvar a condição inicial
+	sort(v, v + n); // ordena os vetores na ordem da menor para a maior
 
 	for(int j = 0 ; j < n ; j++){
 		comp = A[j] - v[j];
@@ -25,17 +26,18 @@ int main(){
 			ordem[j] = A[j];
 			contagem += 1;
 		}
-	}
+	}// Usando o valor comp, aqui salvamos a diferença da altura da boneca na posição j depois de colocar em ordem crescente e antes
+	// Caso o valor seja diferente de 0, a boneca dessa posição mudou de lugar. Logo, salva-se a altura dessa boneca no vetor ordem e aumenta a contagem em 1.
 
-	sort(ordem, ordem + 100100);
+	sort(ordem, ordem + 100100); // Ordena as alturas das bonecas que tiveram que ser mudadas
 
-	cout<<contagem<<endl;
+	cout<<contagem<<endl; // imprime quantas mudanças tiveram que ser feitas
 
 	for(int k = 0 ; k < 100100 ; k++){
 		if(ordem[k] != 0){
 			cout<<ordem[k]<<" ";
 		}
-	}
+	}// imprime a altura das bonecas em ordem crescente.
 
 
 
