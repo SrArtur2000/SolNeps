2bae1b8fbf9205c0297125ae08980b76428d2e19
SrArtur2000 <61886899+SrArtur2000@users.noreply.github.com>
2020-08-04 20:51:30 -0300

    Update sol2.cc

--- sol2.cc
+++ sol2.cc
@@ -10,14 +10,16 @@ int contagem[9];
 
 for(int a = 0 ; a < 10 ; a++){
 	contagem[a] = 0;
-}
+}// começa a contagem em 0
 
 for (int i = 0 ; i < N ; i++){
 	cin>>A;
 	for(int j = 0 ; j < A.size() ; j++){
 		contagem[A[j] - '0']++;
 	}	
-}
+}// Como A[j] é uma string, subtraí-la de um valor decimal da tabela ASCII faz a contagem trata-la como um inteiro.
+// Subtraindo por 0, A[j] - '0' vai ter valores entre 0 e 9. 
+//Sendo assim, essa linha soma 1 para a contagem[(Insira numero entre 0 e 9 aqui)] baseado no valor que A[j] tem na string
 
 for(int l = 0 ; l < 10 ; l++){
 	if(l != 9){
@@ -25,7 +27,7 @@ for(int l = 0 ; l < 10 ; l++){
 }else if(l == 9){
 	cout<<"9"<<" - "<<contagem[9];
 }
-}
+}// imprime tudo, deixando o ultimo sem dar end line;
 
 
 
