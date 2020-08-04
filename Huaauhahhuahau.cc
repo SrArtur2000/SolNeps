c5bee7a2d023ae37902b980c1147a1feea0426a3
SrArtur2000 <61886899+SrArtur2000@users.noreply.github.com>
2020-08-04 20:28:30 -0300

    Update Huaauhahhuahau.cc

--- Huaauhahhuahau.cc
+++ Huaauhahhuahau.cc
@@ -8,7 +8,7 @@ int contagem;
 int tamanho = 0;
 contagem = 0;
 
-\\ Começando as strings vazias. VD são as vogais na ordem direta e VI na ordem inversa
+// Começando as strings vazias. VD são as vogais na ordem direta e VI na ordem inversa
 	
 string vi = "";
 string vd = "";
@@ -22,24 +22,24 @@ for(int i = 0 ; i < risada.size() ; i++){
 		tamanho = tamanho + 1;
 		vd += risada[i];
 	}
-} \\ Isso é pra adicionar as vogais de risada na ordem direta, isto é, da esquerda para a direita. Salva o tamanho da string vi e vd, que é o mesmo.
+} // Isso é pra adicionar as vogais de risada na ordem direta, isto é, da esquerda para a direita. Salva o tamanho da string vi e vd, que é o mesmo.
 
 for (int j = tamanho-1 ; j >= 0 ; j--){
 	vi += vd[j];
-} \\ Aqui para adicionar as vogais para vi, porém na ordem inversa. Lendo a risada da direita para a esquerda,
+} // Aqui para adicionar as vogais para vi, porém na ordem inversa. Lendo a risada da direita para a esquerda,
 
 for (int k = 0 ; k < tamanho ; k++){
 	if (vi[k] == vd[k]){
 		contagem = contagem + 1;
 	}
-} \\ percorre as strings no seu tamanho e conta quantas vezes os elementos em seus respectivos espaços são iguais.
+} // percorre as strings no seu tamanho e conta quantas vezes os elementos em seus respectivos espaços são iguais.
 
 
 if(contagem == tamanho){
 	cout<<"S"<<endl;
 }else{
 	cout<<"N"<<endl;
-} \\ Se a contagem do passo passado for igual ao tamanho, isto é, os vetores forem iguais, imprime S
+} // Se a contagem do passo passado for igual ao tamanho, isto é, os vetores forem iguais, imprime S
 
 return 0;
 }
