f75ec225f358dc41d1f0ab496bde96f6dcdeaa63
SrArtur2000 <61886899+SrArtur2000@users.noreply.github.com>
2020-08-04 20:42:09 -0300

    Update overflow.cc

--- overflow.cc
+++ overflow.cc
@@ -7,10 +7,10 @@ int N;
 int P;
 char op;
 int Q;
-cin>>N;
-cin>>P;
-cin>>op;
-cin>>Q;
+cin>>N; // Maximo que o computador consegue expressar
+cin>>P; // primeiro número a ser operacionado
+cin>>op; // OPeração a ser usada
+cin>>Q; // segundo numero a ser operacionado
 
 if(op == '*'){
 	if(P*Q > N){
@@ -18,14 +18,14 @@ if(op == '*'){
 	}else{
 	cout<<"OK"<<endl;
 	}
-}
+} // Caso a operação seja multiplicação, checar se P vezes Q é maior que N para ver se da overflow ou se é OK
 if(op == '+'){
 	if(P+Q > N){
 		cout<<"OVERFLOW"<<endl;
 	}else{
 	cout<<"OK"<<endl;
 	}
-}
+} // Caso a operação seja soma, checar se P mais Q é maior que N para ver se da overflow ou se é OK
 
 
 return 0;
