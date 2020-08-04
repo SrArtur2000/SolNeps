b16d508dd322bb5d6bb01ed141315e25761d162b
SrArtur2000 <61886899+SrArtur2000@users.noreply.github.com>
2020-08-04 20:32:45 -0300

    Update hanoi.cc

--- hanoi.cc
+++ hanoi.cc
@@ -10,7 +10,7 @@ int hanoi(int N){
 	}else{
 		return 2*hanoi(N-1) + 1;
 	}	
-}
+}// define os casos iniciais da função recursiva da solução matematica das torres de hanoi
 
 int main(){
 int n;
@@ -21,11 +21,11 @@ while(n != 0){
 	teste += 1;
 	v[teste] = n;
 	cin>>n;
-}
+}// adiciona, para o teste 1, quantos cilindros terão para resolver a torre de hanoi
 
 for(int i = 1 ; i <= teste ; i++){
 	cout<<"Teste "<<i<<endl<<hanoi(v[i])<<endl;
-}
+}// imprime todas as linhas de teste de uma vez
 
 return 0;
 }
