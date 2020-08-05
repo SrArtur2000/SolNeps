9eb6ecc947526eeb5ebd0700ddffdf578cc40f80
SrArtur2000 <61886899+SrArtur2000@users.noreply.github.com>
2020-08-05 13:52:33 -0300

    Update dentista.cc

--- dentista.cc
+++ dentista.cc
@@ -5,11 +5,11 @@ using namespace std;
 struct cliente{
 	int inicio;
 	int final;
-};
+}; // struct guardando começo e fim das consultas
 
 bool comparacliente(cliente x, cliente y){
 	return x.final < y.final;
-}
+} //ordena por ordem crescente dos finais das consultas
 
 int main(){
 	cliente pessoa[10010];
@@ -20,16 +20,16 @@ int main(){
 	for(int i = 0 ; i < n ; i++){
 		cin>>pessoa[i].inicio;
 		cin>>pessoa[i].final;
-	}
+	} // recebe os valores do vetor
 
-	sort(pessoa, pessoa + n, comparacliente);
+	sort(pessoa, pessoa + n, comparacliente); // ordena o vetor
 
 	for(int j = 1; j < n ; j++){
 		if(pessoa[j].inicio >= pessoa[j-1].final){
-			total = total + 1;
+			total = total + 1; // Adiciona 1 ao total caso a pessoa j seja valida para ser atendida
 		}else if(pessoa[j].inicio < pessoa[j-1].final){
 			pessoa[j].inicio = pessoa[j-1].inicio;
-			pessoa[j].final = pessoa[j-1].final;
+			pessoa[j].final = pessoa[j-1].final; // caso não, substitui a pessoa j no vetor pela que está sendo atendida
 		}
 	}
 
