0909cb2d17104df7daeda232a3b701f44d27b19f
SrArtur2000 <61886899+SrArtur2000@users.noreply.github.com>
2020-08-21 21:13:15 -0300

    Update and rename ogros to ogros.cc

---
+++ ogros.cc
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+	int n,m;
+	int faixa[10010];
+	int pont[10010];
+	int F[10010];
+
+
+int bb(int x){
+
+	int ini = 0, fim = n-1, meio;
+	if(x >= faixa[fim]){
+		return fim;
+	} // Caso a força seja maior que o maior valor possivel, retorne a posição maior
+	while(ini <= fim){
+		meio = (ini + fim)/2;
+		if(meio == fim){
+			return fim; // Caso tenha chegado ao final
+		}else if(meio == ini){
+			return ini; // caso tenha chegado ao inicio
+		}else{
+			if((x < faixa[meio] and x > faixa[meio - 1]) or x == faixa[meio -1]){ 
+				return meio-1;
+			} // Se estiver em um intervalo, retorne a posição do menor valor do intervalo
+     		if(x < faixa[meio] and x < faixa[meio - 1]){ 
+    	 		fim=meio; // Caso seja menor, aproxime o fim do inicio
+    	 	}
+   	  		if(x > faixa[meio] and x > faixa[meio - 1]){ 
+     			ini=meio; // caso sejam maior, aproxime o inicio do fim
+     		}
+     		if(x == faixa[meio]){
+     			return meio; //Se for igual ao valor, ele estará no intervalo referente a posiço do valor
+     		}
+     	}
+
+	}
+}
+
+int main(){
+	cin>>n>>m;
+	faixa[0] = 0;
+
+	for(int i = 1 ; i < n ; i++){
+		cin>>faixa[i];
+	}
+	for(int j = 0 ; j < n ; j++){
+		cin>>pont[j];
+	}
+	for(int k = 0 ; k < m ; k++){
+		cin>>F[k];
+	}
+	for(int a = 0 ; a < m ; a++){
+		cout<<pont[bb(F[a])]<<" ";	
+	}
+
+	return 0;
+}
