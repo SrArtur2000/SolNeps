c2865c9b1953dc9c3e0fe1b3d5ded929b77ff7a3
SrArtur2000 <61886899+SrArtur2000@users.noreply.github.com>
2020-09-13 08:59:47 -0300

    Update papa.cc

--- papa.cc
+++ papa.cc
@@ -7,56 +7,43 @@
 	struct caixa{
 		int P;
 		int R;
-		int c[1010];
 	};
 
 	caixa C[1010];
-	int contagem[1010];
+	int dp[1010];
 
 	bool comparacaixa(caixa a, caixa b){
-		if(a.R - a.P != b.R - b.P){
-			return a.R - a.P < b.R - b.P;
-		}else if(a.R - a.P == b.R - a.P){
 			return a.R < b.R;
 		}
-	} // ordena pela capacidade - peso, depois pela resistencia. Caso empate nos dois, os pesos são consequentemente iguais
-
-	int dp(int n){
-		int peso = 0;
-		for(int x = 0 ; x < n ; x++){ // Analisa a torre x partindo da caixa x
-			contagem[x] = 0;
-			for(int i = x ; i < n; i++){
-				if(C[i].c[x] >= 0){// Caso a caixa i suporte seu peso + a do resto da torre x
-				peso += C[i].P;
-				contagem[x] += 1;
-					for(int j = i + 1 ; j < n ; j++){ // Remova de cada caixa seguinte no vetor o peso da caixa recem adicionada
-						C[j].c[x] -= C[i].P;
-					}
-				}
-			}
-		}
-			sort(contagem, contagem + n); // ordena contagem
-		return contagem[n-1]; // pega a maior contagem
-	}
+	} // ordena pela resistencia
 
 
 
 	int main(){
 		cin>>n;
+		for(int a = 0 ; a <= n ; a++){
+			pd[a] = 0x3f3f3f3f;
+		}
 
 		for(int i = 0 ; i < n ; i++){
 			cin>>C[i].P;
 			cin>>C[i].R;
-			for(int j = 0 ; j < n ; j++){
-				C[i].c[j] = C[i].R - C[i].P;
-			}
-			
 		}
 
 		sort(C, C + n, comparacaixa);
-
-		cout<<dp(n);
-
-
+		pd[0] = 0;
+		int maximo = 0;
+		
+		for(int j = 0 ; j < n ; j++){
+			for(int k = n ; k >= 0 ; k--){
+				if( (pd[k]+C[j].P) <= C[j].R){ // Ver se é possível colocar a caixa embaixo da torre de k caixas
+					if((pd[k] + C[j].P) < pd[k+1]){ // Vê se é vantajoso colocar essa caixa em relação à torre já existente com k+1 caixas
+					pd[q+1] = (pd[k] + C[j].P);
+					maximo = max(maximo, q+1);
+					}
+				}
+			}	
+		}
+		cout<<maximo;
 		return 0;
 	}
